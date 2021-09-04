fb2077b207dd9fc4fc090e5919222e33ff73b465
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-09-04 10:15:35 +0600

    work report and sell report page

--- admin/changePass.php
+++ admin/changePass.php
@@ -7,17 +7,68 @@
     // start login session
     session_start();
     if (isset($_SESSION['is_adminLogin'])) {
-        // $_SESSION['aEmail'] = $aemail;
+        $aEmail = $_SESSION['aEmail'];
     } else {
         echo "<script>location.href='adminLogin.php';</script>";
     } // end login session
+
+    // Start show record in form
+    $sql = "SELECT a_email, a_password FROM admin_login_tb where a_email = '$aEmail'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $showPassword = $row['a_password'];
+    } // end show record in form
+
+    // Start update record form to database
+    if (isset($_REQUEST['aPassword'])) {
+        $aPass = $_REQUEST['aPassword'];
+        if ($aPass==$showPassword) {
+            $msg = "<div class='alert alert-warning alert-dismissible fade show' role='alert'>Enter a New Password.
+                <button type='button' class='btn-close' data-bs-dismiss = 'alert' aria-label='close'></button>
+            </div>";
+        } elseif ($aPass=='') {
+            $msg = "<div class='alert alert-danger alert-dismissible fade show' role='alert'>Password Fields Are Required.
+                <button type='button' class='btn-close' data-bs-dismiss = 'alert' aria-label='close'></button>
+            </div>";
+        } else {
+            $sql = "UPDATE admin_login_tb SET a_password = '{$aPass}' where a_email = '{$aEmail}'";
+                if ($conn->query($sql) == true) {
+                $msg = "<div class='alert alert-success alert-dismissible fade show' role='alert'>Password Updated Successfully.
+                    <button type='button' class='btn-close' data-bs-dismiss = 'alert' aria-label='close'></button>
+                </div>";
+                } else {
+                $msg = "<div class='alert alert-warning alert-dismissible fade show' role='alert'> Unable To Password Updated.
+                    <button type='button' class='btn-close' data-bs-dismiss = 'alert' aria-label='close'></button>
+                </div>";
+                }     
+        }
+    } // End update record form to database
 
 ?>
 
 <!-- header part -->
 <?php include('adminHead.php'); ?>
 <!-- Start changePass part -->
-
-<!-- End changePass part -->
+<!-- 2nd col Start changePass part -->
+<div class="col-sm-6 pt-5">
+    <form class="mx-5" action="" method="post">
+    <?php if (isset($msg)) {echo $msg;}?>
+        <div class="form-group">
+            <label class="fw-50" for="aEmail">Email</label>
+            <input class="form-control" type="email" name="aEmail" id="aEmail" value="<?= $aEmail; ?>" readonly>
+        </div>
+        <div class="form-group mt-3">
+            <label class="fw-50" for="aPassword">Password</label>
+            <input class="form-control" type="password" name="aPassword" id="aPassword">
+        </div>
+        <div class="form-group mt-4">
+            <input class=" btn btn-danger" type="submit" name="passUpdate" value="Update" id="passUpdate">
+            <input class=" btn btn-secondary" type="reset" name="passReset" value="Reset" id="passReset">
+        </div>
+    </form>
+</div>
+<!-- 2nd col End changePass part -->
 <!-- footer part -->
 <?php include('adminFooter.php'); ?>
--- admin/sellReport.php
+++ admin/sellReport.php
@@ -17,7 +17,88 @@
 <!-- header part -->
 <?php include('adminHead.php'); ?>
 <!-- Start sellReport part -->
+<!-- start 2nd column-->
+<div class="col-sm-8 col-xl-10 col-md-9 mt-5">
+    <form class="d-print-none mx-5" method="post">
+        <div class="row">
+            <div class="form-group col-md-4 col-lg-3">
+                <input class="form-control" type="date" name="start_date" id="start_date">
+            </div>
+            <div class="col-md-1 col-lg-1 text-center">
+                <span class=""> To </span>
+            </div>
+            <div class="form-group col-md-4 col-lg-3">
+                <input class="form-control" type="date" name="end_date" id="end_date">
+            </div>
+            <div class="form-group col-md-3 col-lg-2 mt-2 mt-md-0">
+                <input class="form-control btn btn-secondary" type="submit" name="search_date" id="search_date" value="Search">
+            </div>
+        </div>
+    </form>
+    <?php
+        if (isset($_REQUEST['search_date'])) {
 
+            $startDate = $_REQUEST['start_date'];
+            $endDate = $_REQUEST['end_date'];
+
+            $sql = "SELECT * FROM sell_product_tb WHERE s_p_date BETWEEN '$startDate' AND '$endDate'";
+        
+            $result = $conn->query($sql);
+        
+            if ($result->num_rows > 0) {
+                
+                echo "<div class='mt-5'>
+                    <table class='table table-bordered table-striped'>
+                        <h3 class='mb-3 text-center'>Sell Details.</h3>
+                        <thead>
+                            <tr>
+                                <th>Customer ID</th>
+                                <th>Customer Name</th>
+                                <th>Customer Address</th>
+                                <th>Customer Mobile</th>
+                                <th>Product Name</th>
+                                <th>Product Quantity</th>
+                                <th>Product Each Price</th>
+                                <th>Product Total Price</th>
+                                <th>Selling Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>";?>
+                        <?php
+                            while($row = $result->fetch_assoc()){
+                            echo "<tr>
+                                <td>{$row['s_c_id']}</td>
+                                <td>{$row['s_c_name']}</td>
+                                <td>{$row['s_c_add']}</td>
+                                <td>{$row['s_c_mobile']}</td>
+                                <td>{$row['s_p_name']}</td>
+                                <td>{$row['s_p_quan']}</td>
+                                <td>{$row['s_p_each_price']}</td>
+                                <td>{$row['s_p_total_price']}</td>
+                                <td>{$row['s_p_date']}</td>
+                            </tr>";
+                            }
+                        ?>
+                    <?php
+                        echo "</tbody>    
+                        
+                    </table>
+        
+                    <div class='d-print-none mx-5 text-end my-5'>
+                        <form class=''>
+                            <input class='btn btn-danger' type='submit' value='Print' onclick='window.print()'>
+                            <a class='btn btn-secondary ms-3' href='sellReport.php'>Close</a>
+                        </form>
+                    </div>
+                
+                </div>";
+            } else {
+                echo "<div class='col-sm-4 mt-5 mx-5 alert alert-danger text-center'> No Search Found </div>";
+            }
+        }
+    ?>
+</div>
+<!-- end 2nd column-->
 <!-- End sellReport part -->
 <!-- footer part -->
 <?php include('adminFooter.php'); ?>
