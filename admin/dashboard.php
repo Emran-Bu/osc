d09d8b7f152a1fe3ca705ba237569dd07b67e045
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-09-04 11:30:48 +0600

    dashboard dynamic

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -12,6 +12,26 @@
         echo "<script>location.href='adminLogin.php';</script>";
     } // end login session
 
+    // start submit request show data
+    $sql = "SELECT max(requester_id) FROM submit_requester_tb";
+    $result = $conn->query($sql);
+    $row = $result->fetch_row();
+    $totalSubReq = $row[0];
+    // end submit request show data
+
+    // start assign request show data
+    $sql = "SELECT max(requester_id) FROM assign_work_tb";
+    $result = $conn->query($sql);
+    $row = $result->fetch_row();
+    $totalSubAssign = $row[0];
+    // end assign request show data
+
+    // start Technician show data
+    $sql = "SELECT * FROM technician_tb";
+    $result = $conn->query($sql);
+    $totalTech = $result->num_rows;
+    // end Technician show data
+
 ?>
 
 <!-- header part -->
@@ -20,29 +40,29 @@
     <div class="col-sm-8 col-xl-10 col-md-9 py-5"> <!-- Start dashboard 2nd column -->
         <div class="row mx-5"> <!-- start first row -->
             <div class="col-md-4 col-12">
-                <div class="card text-light text-center bg-primary mb-3" style="">
+                <div class="card text-light text-center bg-primary mb-3">
                     <div class="card-header">Requests Received</div>
                     <div class="card-body">
-                        <div class="card-title">43</div>
-                        <a class="btn text-light" href="#">View</a>
+                        <div class="card-title"><?= $totalSubReq ?></div>
+                        <a class="btn text-light" href="request.php">View</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4 col-12">
-                <div class="card text-light text-center bg-success mb-3" style="">
+                <div class="card text-light text-center bg-success mb-3">
                     <div class="card-header">Assigned Work</div>
                     <div class="card-body">
-                        <div class="card-title">53</div>
-                        <a class="btn text-light" href="#">View</a>
+                        <div class="card-title"><?= $totalSubAssign ?></div>
+                        <a class="btn text-light" href="workOrder.php">View</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4 col-12">
-                <div class="card text-light text-center bg-warning mb-3" style="">
+                <div class="card text-light text-center bg-warning mb-3">
                     <div class="card-header">No. of Technician</div>
                     <div class="card-body">
-                        <div class="card-title">30</div>
-                        <a class="btn text-light" href="#">View</a>
+                        <div class="card-title"><?= $totalTech ?></div>
+                        <a class="btn text-light" href="technician.php">View</a>
                     </div>
                 </div>
             </div>
